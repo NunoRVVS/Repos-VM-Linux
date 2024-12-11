793c96ac0057f1ba62a2b7bb6a9d8442cdd4508b
Nunorvvs <nunor@Xubuntu>
2024-12-11 12:33:33 +0000

    test

--- repos/Rock_Paper_Scissors/Rock_Paper_Scissors-JS/script.js
+++ repos/Rock_Paper_Scissors/Rock_Paper_Scissors-JS/script.js
@@ -7,7 +7,27 @@ function getComputerChoice() {
 }
 
 function checkWinner(humanSelection, computerSelection) {
-    if(humanSelection == computerSelection) {
+    const div = document.querySelector("div");
+    const btnRock = document.createElement("button");
+    btnRock.classList.add("btnRock");
+    btnRock.textContent = "rock";
+    const btnPaper = document.createElement("button");
+    btnPaper.classList.add("btnPaper");
+    btnPaper.textContent = "paper";
+    const btnScissors = document.createElement("button");
+    btnScissors.classList.add("btnScissors");
+    btnScissors.textContent = "scissors";
+    div.appendChild(btnRock);
+    div.appendChild(btnPaper);
+    div.appendChild(btnScissors);
+   
+    console.log(div);
+
+    const buttons = document.querySelectorAll("button");
+
+    buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+     if(humanSelection == computerSelection) {
         return "Tie!";
     } 
     else if(
@@ -20,7 +40,10 @@ function checkWinner(humanSelection, computerSelection) {
     else {
         return "Computer";
     }
+    });
+});
 }
+console.log(checkWinner());
 
 function playRound(humanSelection, computerSelection) {
     const result = checkWinner(humanSelection, computerSelection);
@@ -55,18 +78,18 @@ console.log(getHumanChoice());
 function game() {
     let scorePlayer = 0;
     let scoreComputer = 0;
-    for(let i = 0; i < 5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        console.log(playRound(humanSelection, computerSelection));
-        console.log("-------------------------------");
-        if(checkWinner(humanSelection, computerSelection) == "Player") { 
-            scorePlayer++;
-        }
-        else if(checkWinner(humanSelection, computerSelection) == "Computer") {
-            scoreComputer++;
-        }
-    }
+    // for(let i = 0; i < 5; i++) {
+    //     const humanSelection = getHumanChoice();
+    //     const computerSelection = getComputerChoice();
+    //     console.log(playRound(humanSelection, computerSelection));
+    //     console.log("-------------------------------");
+    //     if(checkWinner(humanSelection, computerSelection) == "Player") { 
+    //         scorePlayer++;
+    //     }
+    //     else if(checkWinner(humanSelection, computerSelection) == "Computer") {
+    //         scoreComputer++;
+    //     }
+    // }
     console.log("Game Over!");
     console.log("-------------------------------");
     if(scorePlayer > scoreComputer) {
