baaadbf9855cd7210b2bd78a70418c6ab0d0411c
Nunorvvs <nunor@Xubuntu>
2024-12-16 14:39:56 +0000

    code refactor1

--- repos/Rock_Paper_Scissors/Rock_Paper_Scissors-JS/script.js
+++ repos/Rock_Paper_Scissors/Rock_Paper_Scissors-JS/script.js
@@ -1,106 +1,95 @@
 const options = ["rock", "paper", "scissors"];
+const winners = [];
 
-function getComputerChoice() { 
-    let computerChoice = options[Math.floor(Math.random() * options.length)];
-    console.log(computerChoice);
-    return computerChoice;
+function game() {
+    for(let i = 0; i < 5; i++) {
+        playRound();
+    }
 }
 
+function playRound() {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    const winner = checkWinner(humanSelection, computerSelection);
+    winners.push(winner);
+}
+
+function getHumanChoice() {
+    let input = prompt("What will you choose: Rock, Paper or Scissors?");
+    while(input == null) { 
+         input = prompt("What will you choose: Rock, Paper or Scissors?");
+    } 
+    input = input.toLowerCase();
+    let check = validateInput(input);
+    while (check == false) {
+        input = prompt(
+            "You need to spell it exactly as: Rock or Paper or Scissors");
+        };
+        while (input == null) {
+        input = prompt("Type either: Rock or Paper or Scissors?"
+        );
+        input = input.toLowerCase();
+        check = validateInput(input);
+    }
+    return input;
+}
+
+
+function getComputerChoice() { 
+    options[Math.floor(Math.random() * options.length)];
+}
+
+function validateInput(option) {
+    return options.includes(option)
+}
+
+
 function checkWinner(humanSelection, computerSelection) {
-    const div = document.querySelector("div");
-    const btnRock = document.createElement("button");
-    btnRock.classList.add("btnRock");
-    btnRock.textContent = "rock";
-    const btnPaper = document.createElement("button");
-    btnPaper.classList.add("btnPaper");
-    btnPaper.textContent = "paper";
-    const btnScissors = document.createElement("button");
-    btnScissors.classList.add("btnScissors");
-    btnScissors.textContent = "scissors";
-    div.appendChild(btnRock);
-    div.appendChild(btnPaper);
-    div.appendChild(btnScissors);
-   
-    console.log(div);
-
-    const buttons = document.querySelectorAll("button");
-
-    buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-     if(humanSelection == computerSelection) {
+    if(humanSelection == computerSelection) {
         return "Tie!";
     } 
     else if(
         (humanSelection == "rock" && computerSelection == "scissors") ||
-        (humanSelection == "scissors" && computerSelection == "paper") ||
-        (humanSelection == "paper" && computerSelection == "rock")
+        (humanSelection == "paper" && computerSelection == "rock") ||
+        (humanSelection == "scissors" && computerSelection == "paper")
     ) { 
         return "User";
     } 
     else {
         return "Computer";
     }
-    });
-});
-}
-console.log(checkWinner());
-
-function playRound(humanSelection, computerSelection) {
-    const result = checkWinner(humanSelection, computerSelection);
-    if(result == "Tie!") {
-        return "It is a Tie!"
-    } 
-    else if(result == "User") {
-        return `You win! ${humanSelection} beats ${computerSelection}!`
-    } 
-    else {
-        return `You loose! ${computerSelection} beats ${humanSelection}!`
-    }
 }
 
-function getHumanChoice() {
-    let validateInput = false;
-    while(validateInput == false) { 
-        const humanChoice = prompt("Do you choose Rock, Paper or Scissors?");
-        if(humanChoice == null) {
-            continue;
-        } 
-        const choiceInLower = humanChoice.toLowerCase();
-//the includes will only validate what is in the array OPTIONS
-        if(options.includes(choiceInLower)) {
-            validateInput = true;
-            return choiceInLower;
-        }
-    }
-}
-console.log(getHumanChoice());
+// function playRound(humanSelection, computerSelection) {
+//     const winner = checkWinner(humanSelection, computerSelection);
+//     if(winner == "Tie!") {
+//         return "It is a Tie!"
+//     } 
+//     else if(winner == "User") {
+//         return `You win! ${humanSelection} beats ${computerSelection}!`
+//     } 
+//     else {
+//         return `You loose! ${computerSelection} beats ${humanSelection}!`
+//     }
+// }
 
-function game() {
-    let scorePlayer = 0;
-    let scoreComputer = 0;
-    // for(let i = 0; i < 5; i++) {
-    //     const humanSelection = getHumanChoice();
-    //     const computerSelection = getComputerChoice();
-    //     console.log(playRound(humanSelection, computerSelection));
-    //     console.log("-------------------------------");
-    //     if(checkWinner(humanSelection, computerSelection) == "Player") { 
-    //         scorePlayer++;
-    //     }
-    //     else if(checkWinner(humanSelection, computerSelection) == "Computer") {
-    //         scoreComputer++;
-    //     }
-    // }
-    console.log("Game Over!");
-    console.log("-------------------------------");
-    if(scorePlayer > scoreComputer) {
-        console.log("You won!");
-    } 
-    else if (scorePlayer < scoreComputer) {
-        console.log("You lost!");
-    } 
-    else {
-        console.log ("It is a tie!");
-    }
+function logWins() {
+    console.log(winners);
+    let humanWins = winners.filter((item) => item == "User").length;
+    let computerWins = winners.filter((item) => item == "Computer").length;
+    let ties = winners.filter((item) => item == "Tie").length;
+    console.log("Results:");
+    console.log("Player Wins:", humanWins);
+    console.log("Computer Wins:", computerWins);
+    console.log("Ties:", ties);
 }
+
+// function logRound(getHumanChoice, getComputerChoice, checkWinner){
+//     console.log("Round:",round)
+//     console.log("Player Chose:",getHumanChoice)
+//     console.log("Computer Chose:",getComputerChoice)
+//     console.log(checkWinner, "won the round")
+// } 
+
 game();
 
